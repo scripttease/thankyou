2a37552e63b50ee3b0b2f1a7e1904ceccf1b3101
Alice <amyalicedee@gmail.com>
2016-09-14 16:32:33 +0100

    Add timeout fn to remove names

--- thankyou.js
+++ thankyou.js
@@ -8,22 +8,30 @@ function getRandomInt(min, max) {
 var winHeight = window.innerHeight;
 var winWidth = window.innerWidth;
 
-// space where words appear
-var wordSpace = document.getElementById("word-space");
+function randomNamesDraw {
+  // space where words appear
+  var wordSpace = document.getElementById("word-space");
 
-function nameDraw() {
-  //select random names
-  var randomNumName = getRandomInt(0, names.length -1);
-  var randomName = names[randomNumName];
+  function nameDraw() {
+    //select random names
+    var randomNumName = getRandomInt(0, names.length -1);
+    var randomName = names[randomNumName];
 
-  // where individual name is placed
-  var namePlace = document.createElement("div");
-  namePlace.textContent = randomName;
-  namePlace.style.position = "absolute";
-  namePlace.style.left = getRandomInt(5, winWidth) + "px";
-  namePlace.style.top = getRandomInt(5, winHeight) + "px";
-  document.body.appendChild(namePlace);
+    // where individual name is placed
+    var namePlace = document.createElement("div");
+    namePlace.textContent = randomName;
+    namePlace.style.position = "absolute";
+    namePlace.style.left = getRandomInt(5, winWidth) + "px";
+    namePlace.style.top = getRandomInt(5, winHeight) + "px";
+    document.body.appendChild(namePlace);
+
+    setTimeout(function() { namePlace.parentNode.removeChild(namePlace); }, 4000);
+
+    setTimeout(function() { nameDraw(); }, 100);
+  };
+
+  nameDraw();
 
 }
 
-nameDraw();
+randomNamesDraw();
